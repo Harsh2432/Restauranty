602762df3e27ae110708d63d9886646ecac6d69e
Harsh2432 <jadhavharsh243@gmail.com>
2022-12-04 15:12:05 +0530

    Added Bootstrap styling to website

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,24 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-
-})
+  app: {
+    head: {
+      meta: [
+        // <meta name="viewport" content="width=device-width, initial-scale=1">
+        { name: "viewport", content: "width=device-width, initial-scale=1" },
+      ],
+      script: [
+        // <script src="https://myawesome-lib.js"></script>
+        {
+          src: "https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js",
+        },
+      ],
+      link: [
+        // <link rel="stylesheet" href="https://myawesome-lib.css">
+        {
+          rel: "stylesheet",
+          href: "https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css",
+        },
+      ],
+    },
+  },
+});
